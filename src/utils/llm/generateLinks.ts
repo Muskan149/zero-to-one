988dc05ffc2496d5a82f5263d3d92e10a721f614
Muskan149 <muskanmahajan2004@gmail.com>
2025-05-27 20:24:25 -0400

    created a new function for generating links

--- src/utils/llm/generateLinks.ts
+++ src/utils/llm/generateLinks.ts
@@ -4,7 +4,3 @@ const options = {
     body: '{"model":"sonar","messages":[{"role":"system","content":"Be precise and concise."},{"role":"user","content":"How many stars are there in our galaxy?"}]}'
   };
   
-  fetch('https://api.perplexity.ai/chat/completions', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
